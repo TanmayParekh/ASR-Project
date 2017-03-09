468fd9174073589c509ec0fffbdcb5a57f28a792
tanmay <tparekh97@gmail.com>
2017-03-09 23:31:37 +0530

    Adding Linguistic Part

---
+++ Linguistic/create_vocab.py
@@ -0,0 +1,89 @@
+import sys
+import os
+import operator
+
+global_vocab = {}					# GLOBAL VOCABULARY LIST
+
+# Find all csv files from the path
+def find_csv_files(dir_path, extension=".csv"):
+	filenames = os.listdir(dir_path)
+	return [filename for filename in filenames if filename.endswith(extension)]
+
+# Remove symbols like ".", ",", etc. from words
+def clean_word(word):
+
+	word = word.replace(",","")
+	word = word.replace(".","")
+	word = word.replace("\"","")
+
+	return word
+
+# Split the transcription into words and add to vocab
+# Also keep a count of the word
+def add_to_vocab(transcription):
+
+	split_trans = transcription.split()
+
+	for word in split_trans:
+
+		# Clean the word first
+		word = clean_word(word)
+
+		if (word in global_vocab.keys()):
+			global_vocab[word] += 1
+		else:
+			global_vocab[word] = 1
+
+# Print the sorted global vocab as csv into a file
+def print_vocab(file):
+
+	f = open(file,'w')
+
+	for word in sorted(global_vocab, key=global_vocab.get, reverse=True):
+		f.write(word + "," + str(global_vocab[word]) + "\n")
+
+	f.close()
+
+# Filter words with condition that word_count >= 10
+def print_extracted_vocab(file):
+
+	f = open(file,'w')
+
+	for word in sorted(global_vocab, key=global_vocab.get, reverse=True):
+
+		if (global_vocab[word] >= 10):
+			f.write(word + "\n")
+		else:
+			break
+
+	f.close()
+
+#####################################
+############ MAIN CODE ##############
+
+dir = "transcriptions"
+files = find_csv_files(dir)
+
+for file in files:
+
+	f = open(dir + '/' + file,'r')
+	lineno = 0
+
+	for line in f:
+
+		# If lineno == 0 -> Do Nothing.
+		# First line is header
+		if (lineno > 0):
+			
+			# Extract the third element (transcription) and add to the global vocabulary
+			split = line.split(";")
+			trans = split[2]
+			add_to_vocab(trans)
+
+		lineno += 1
+
+	f.close()
+
+# Print the vocabulary
+print_vocab("vocab.csv")
+print_extracted_vocab("extracted_vocab.txt")
---
+++ Linguistic/make_feature.py
@@ -0,0 +1,99 @@
+import sys 
+import os
+
+# Find all csv files from the path
+def find_csv_files(dir_path, extension=".csv"):
+	filenames = os.listdir(dir_path)
+	return [filename for filename in filenames if filename.endswith(extension)]
+
+# Remove symbols like ".", ",", etc. from words
+def clean_word(word):
+
+	word = word.replace(",","")
+	word = word.replace(".","")
+	word = word.replace("\"","")
+
+	return word
+
+# Write header (feature names) into file
+def write_header(file, vocab):
+
+	f = open(file,'w')
+	header = "file_name,start_time,end_time"
+
+	for word in vocab:
+		header += "," + word
+
+	header += ",sentiment\n"
+	f.write(header)
+
+	f.close()
+
+# Add a feature corresponding to each line in a file
+def build_feature(line, dataset_file, feature_file, vocab_list):
+
+	f = open(feature_file,'a')
+
+	# Extract features from line
+	split_line = line.split(';')
+	start_time = split_line[0]
+	end_time = split_line[1]
+	trans = split_line[2]
+	sentiment = split_line[3]
+
+	# Extract words in trans and clean them
+	trans_split = trans.split()
+	for word in trans_split:
+		word = clean_word(word)
+
+	# Build feature count for the words extracted above
+	feature_count = []
+	for feature in vocab_list:
+		feature_count.append(trans_split.count(feature))
+
+	# Create final feature vector
+	feature_line = dataset_file + "," + start_time + "," + end_time
+	for count in feature_count:
+		feature_line += "," + str(count)
+	feature_line += "," + sentiment + "\n"
+
+	# Write to file
+	f.write(feature_line)
+
+	f.close()
+
+
+############################
+######## MAIN CODE #########
+
+final_file = "final_feature.csv"
+
+# Make a list of extracted words
+vocab_file = open("extracted_vocab.txt")
+extr_vocab = []
+
+for line in vocab_file:
+	extr_vocab.append(line[:-1])
+
+# Write header
+write_header(final_file, extr_vocab)
+
+dir = "transcriptions"
+files = find_csv_files(dir)
+
+for file in files:
+
+	f = open(dir + '/' + file,'r')
+	lineno = 0
+
+	for line in f:
+
+		# If lineno == 0 -> Do Nothing.
+		# First line is header
+		if (lineno > 0):
+			
+			build_feature(line,file,final_file,extr_vocab)
+
+		lineno += 1
+
+	f.close()
